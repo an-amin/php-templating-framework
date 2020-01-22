4bf403ae675c181d83e75996e1ce48e8f64b871f
Aminul Islam <hello-amin@yandex.com>
2020-01-22 13:31:49 +0600

    pages

--- template/default/pages/index.php
+++ template/default/pages/index.php
@@ -4,8 +4,7 @@
 	<div class="card mt-5">
 		<div class="card-body">
 			<p class="display-3 text-center">
-				<i class="fa fa-warning fa-lg"></i><br>
-				<b>404</b>
+				hello world
 			</p>
 		</div>
 	</div>
